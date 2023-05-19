57717a4274be3c1993188453fb93298699572764
Hitori <113356370+Hitori638@users.noreply.github.com>
2023-05-19 11:38:17 +0200

    script that fetches the name of the winner into an array

---
+++ TextSerializer.cs
@@ -0,0 +1,49 @@
+﻿using UdonSharp;
+using UnityEngine;
+using TMPro;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TextSerializer : UdonSharpBehaviour
+{
+    [SerializeField] private TextMeshProUGUI[] textObjects;
+    [SerializeField] private TextMeshPro newTMProText;
+
+    private string[] serializedTextArray;
+
+    private void Start()
+    {
+        serializedTextArray = new string[textObjects.Length];
+    }
+
+    public override void OnDeserialization()
+    {
+        for (int i = 0; i < textObjects.Length; i++)
+        {
+            if (textObjects[i] != null)
+            {
+                serializedTextArray[i] = textObjects[i].text;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        bool textChanged = false;
+
+        for (int i = 0; i < textObjects.Length; i++)
+        {
+            if (textObjects[i] != null && textObjects[i].text != serializedTextArray[i])
+            {
+                serializedTextArray[i] = textObjects[i].text;
+                Debug.Log("Text #" + (i + 1) + " changed to: " + serializedTextArray[i]);
+                textChanged = true;
+            }
+        }
+
+        if (textChanged && newTMProText != null)
+        {
+            newTMProText.text = string.Join("\n", serializedTextArray);
+        }
+    }
+}
